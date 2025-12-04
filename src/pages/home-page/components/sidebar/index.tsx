1a5b54b7c42bf08022629cff77a4790178e40619
ddmuzyk <domino2k1@gmail.com>
2025-12-04 18:41:54 +0100

    change button style based on path

--- src/pages/home-page/components/sidebar/index.tsx
+++ src/pages/home-page/components/sidebar/index.tsx
@@ -1,8 +1,9 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { colors } from "src/constants/colors";
 import { NavLink } from "react-router";
 import { routes } from "src/constants/routes"
 import { defaultButtonStyles } from "src/constants/styles";
+import { useLocation } from "react-router";
 
 const sideBarRoutes = [
   { name: 'Home', path: routes.home },
@@ -14,14 +15,13 @@ const S = {
     width: 215px;
     padding: 8px;
     background-color: ${colors.navy[2]};
-    /* height: 100%; */
   `,
   Nav: styled.nav`
     display: flex;
     flex-direction: column;
     gap: 8px;
   `,
-  NavLink: styled(NavLink)`
+  NavLink: styled(NavLink)<{ isSelected?: boolean }>`
     ${defaultButtonStyles};
     text-decoration: none;
     color: ${colors.white};
@@ -30,14 +30,24 @@ const S = {
     &:hover {
       color: ${colors.white};
     }
+
+    ${({ isSelected }) => isSelected && css`
+      background-color: ${colors.blue[1]};
+      &:hover {
+        background-color: ${colors.blue[1]};
+      };
+
+    `}
   `,
 };
 
 export const Sidebar = () => {
+  const location = useLocation();
+
   return <S.Sidebar>
     <S.Nav>
       {sideBarRoutes.map(({ name, path }) => (
-        <S.NavLink key={path} to={path}>
+        <S.NavLink key={path} to={path} isSelected={path === location.pathname}>
           {name}
         </S.NavLink>
       ))}
